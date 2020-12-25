ba29bba4913d994e8510543db472b16a939817d0
George Cover <5285122+TheOptimist@users.noreply.github.com>
2020-12-25 13:11:47 -0500

    Setting network identiying names

--- nix/darwin/configuration.nix
+++ nix/darwin/configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
-{
+let
+  user_name = "george";
+
+in {
   # List packages installed in system profile. To search by name, run:
   environment.systemPackages = with pkgs; [
     bash
@@ -16,11 +19,17 @@
   environment.loginShell = pkgs.zsh;
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nix;
 
   programs.bash.enable = true;  
   programs.zsh.enable = true;
   programs.fish.enable = true;
+
+  networking = {
+    hostName = "${user_name}-macbookpro";
+    computerName = "${user_name}-macbookpro";
+    localHostName = "${user_name}-macbookpro";
+  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   system.stateVersion = 4;
